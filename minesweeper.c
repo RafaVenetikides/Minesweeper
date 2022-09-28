ff9e9adc3fa134d4f8db30ba19559ab24ba55d2f
maturidadejpg <rafavenetikides@gmail.com>
2022-09-28 17:33:51 -0300

    Otimizações e outras coisas

--- minesweeper.c
+++ minesweeper.c
@@ -10,13 +10,13 @@ Dificil = 16x30(99 bombas)
 #define COL 30
 
 void menu();
-int criaMatriz(int level);
-void mostraCampo(int campo[LIN][COL]);
-void mostraTela(char tela[LIN][COL], int level);
-void preencheCampo(int, int, int campo[LIN][COL]);
-void preencheTela(int level, char tela[LIN][COL]);
-int abreCasas(int campo[LIN][COL], char tela[LIN][COL], int x, int y, int k);
-void criaBombas(int campo[LIN][COL], int level);
+int criaMatriz(int);
+void mostraCampo(int [LIN][COL]);
+void mostraTela(char [LIN][COL], int, int);
+void preencheCampo(int, int, int [LIN][COL]);
+void preencheTela(int , int, int, char [LIN][COL]);
+int abreCasas(int [LIN][COL], char [LIN][COL], int, int, int);
+void criaBombas(int [LIN][COL], int, int, int);
 
 int main(){
     int level;  //dificuldade selecionada
@@ -26,6 +26,7 @@ int main(){
     int y;  //coordenada y selecionada
     int row;
     int coll;
+    int bombas;
     int k;  //booleano que mantém o jogo rodando
     k = 0;
 
@@ -37,20 +38,23 @@ int main(){
         case 1:
             row = 9;
             coll = 9;
+            bombas = 10;
             break;
         case 2:
             row = 16;
             coll = 16;
+            bombas = 40;
             break;
         case 3:
             row = 16;
             coll = 30;
+            bombas = 99;
             break;
         }
 
         preencheCampo(row, coll, campo);
-        preencheTela(level, tela);
-        criaBombas(campo, level);
+        preencheTela(level,row, coll, tela);
+        criaBombas(campo, row, coll, bombas);
 
         while (k == 0){
             x = 0;
@@ -58,7 +62,7 @@ int main(){
             printf("Campo: \n");
             mostraCampo(campo);
             printf("Tela: \n");
-            mostraTela(tela, level);
+            mostraTela(tela, row, coll);
 
             printf("Selecione a coordenada x: ");
             scanf("%d", &x);
@@ -81,34 +85,18 @@ void menu(){    //Printar menu
     printf("\n    1 - I'm too young to die.\n    2 - Hurt me plenty.\n    3 - Ultra-Violence.\n    0 - Sair\n");
 }
 
-void preencheTela(int level, char tela[LIN][COL]){  //Preenche a tela mostrada ao jogador
+void preencheTela(int level,int row, int coll, char tela[LIN][COL]){  //Preenche a tela mostrada ao jogador
     int i;  //linhas
     int j;  //colunas
-    if (level == 1){
-        for (i = 0; i <= (LIN -1); i++){
-            for (j = 0; j<= (COL -1); j++){
-                if (i <= 8 && j <= 8){
-                    tela[i][j] = ("%s", 45);
-                    }
-                }
-            }
-        }
-    if (level == 2){
-        for (i = 0; i <= (LIN -1); i++){
-            for (j = 0; j<= (COL -1); j++){
-                if (i <= 15 && j <= 15){
-                    tela[i][j] = ("%s", 45);
-                }
-                }
-            }
-        }
-    if (level == 3){
-        for (i = 0; i <= (LIN -1); i++){
-            for (j = 0; j<= (COL -1); j++){
+    
+    for (i = 0; i <= (LIN -1); i++){
+        for (j = 0; j<= (COL -1); j++){
+            if (i < row && j < coll){
                 tela[i][j] = ("%s", 45);
             }
         }
     }
+        
 }
 
 void preencheCampo(int row, int coll, int campo[LIN][COL]){   //Preenche o campo interpretado pelo computador
@@ -139,32 +127,22 @@ void mostraCampo(int campo[LIN][COL]){  //Printa o campo interpretado pelo compu
     }
 }
 
-void mostraTela(char tela[LIN][COL], int level){   //Printa a tela mostrada ao jogador
+void mostraTela(char tela[LIN][COL], int row, int coll){   //Printa a tela mostrada ao jogador
     int i;  //linhas
     int j;  //colunas
     for (i = 0; i <= (LIN -1); i++){
         for (j = 0; j<= (COL -1); j++){
-            if (level == 1){
-                if (i >= 9 || j >= 9){
+                if (i >= row || j >= coll){
                 printf("\0");
+                }
+                else if (j == 0){
+                    printf("%2d | [%1c]", (i+1), tela[i][j]);
                 }
                 else{
                 printf("[%1c]", tela[i][j]);
                 }
-            }
-            if (level == 2){
-                if (i >= 16 || j >= 16){
-                printf("\0");
-                }
-                else{
-                printf("[%1c]", tela[i][j]);
-                }
-            }
-            if (level == 3){
-                printf("[%1c]", tela[i][j]);
-            }
         }
-        printf("\n");
+    printf("\n");
     }
 }
 
@@ -179,44 +157,19 @@ int abreCasas(int campo[LIN][COL], char tela[LIN][COL], int x, int y, int k){   
     }   
 }
 
-void criaBombas(int campo[LIN][COL], int level){  //posiciona as bombas no campo
+void criaBombas(int campo[LIN][COL], int row, int coll, int bombas){  //posiciona as bombas no campo
     int bombax; //coordenada x da bomba
     int bombay; //coordenada y da bomba
     int i;  //numero de bombas
-    if (level == 1){
-        for (i = 0; i< 10; i++){
-            bombax = rand() % 9;
-            bombay = rand() % 9;
-            if (campo[bombax][bombay] == -1){
-                i--;
-            }
-            else{
-                campo[bombax][bombay] = -1;
-            }
+
+    for (i = 0; i< bombas; i++){
+        bombax = rand() % row;
+        bombay = rand() % coll;
+        if (campo[bombax][bombay] == -1){
+            i--;
         }
-    }
-    if (level == 2){
-        for (i = 0; i< 40; i++){
-            bombax = rand() % 16;
-            bombay = rand() % 16;
-            if (campo[bombax][bombay] == -1){
-                i--;
-            }
-            else{
-                campo[bombax][bombay] = -1;
-            }
-        }
-    }
-    if (level == 3){
-        for (i = 0; i< 99; i++){
-            bombax = rand() % 16;
-            bombay = rand() % 30;
-            if (campo[bombax][bombay] == -1){
-                i--;
-            }
-            else{
-                campo[bombax][bombay] = -1;
-            }
+        else{
+            campo[bombax][bombay] = -1;
         }
     }
 }
